85b253a1ffa2b194a5922f70c27e1a78296e3bef
SHUBHAMAGRAWAL9414 <140111040+SHUBHAMAGRAWAL9414@users.noreply.github.com>
2024-11-13 01:32:56 +0530

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,20 @@
+# import  shubham 
+# shubham.add (10, 20)
+# shubham.sub (22,11)
+# print(shubham.n1)
+
+# import  shubham  as s
+# s.add (10, 20)
+# s.sub (22,11)
+# print(s.n1)
+
+# from shubham import add, sub, n1 
+# add (10, 20)
+# sub (22,11)
+# print(n1)
+
+from shubham import *
+add (10, 20)
+sub (22,11)
+print(n1)
+
